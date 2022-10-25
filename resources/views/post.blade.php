ef64207e5f7e9fe84163c18912fb0340d4cebf93
dimasprayuda212 <dimassprayuda212@gmail.com>
2022-10-25 20:12:22 +0700

    menyelesaikan video 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
     <article>
         <h2>{{ $post->title }}</h2>
 
+        <p>By. Dimas Prayuda in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
         {!! $post->body !!}
 
     </article>
