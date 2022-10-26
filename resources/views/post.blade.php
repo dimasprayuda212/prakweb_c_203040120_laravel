dcdc6d77b0e6589e556617c7d279e1986cebaa2a
dimasprayuda212 <dimassprayuda212@gmail.com>
2022-10-26 12:54:02 +0700

    menyelesaikan video 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,13 +2,13 @@
 
 @section('container')
     <article>
-        <h2>{{ $post->title }}</h2>
+        <h2 class="mb-5">{{ $post->title }}</h2>
 
-        <p>By. Dimas Prayuda in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
 
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
